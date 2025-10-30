c3dd43e69b30e5ee163221a1330620b83080744f
Woody Lin <wancheng.lin001@gmail.com>
2025-10-30 16:12:39 -0400

    Update PropDrilling.jsx

--- react-features/src/components/PropDrilling/PropDrilling.jsx
+++ react-features/src/components/PropDrilling/PropDrilling.jsx
@@ -1,10 +1,30 @@
-import React from "react";
 import Navigation from "../Navigation";
+// import { Link } from "react-router";
+
+function Grandchild({ user }) {
+  return (
+    <div>
+      <p>Hello, {user}!</p>
+      <p>Using prop drilling from parent to grandchild</p>
+    </div>
+  );
+}
+
+function Child({ user }) {
+  return <Grandchild user={user} />;
+}
+
+function Parent() {
+  const user = "Woody Lin";
+
+  return <Child user={user} />;
+}
 
 const PropDrilling = () => {
   return (
     <div>
       <Navigation />
+      <Parent />
     </div>
   );
 };
