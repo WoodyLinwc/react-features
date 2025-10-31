c2cbafdc3ace51cdf1754503ff27dd627e930728
Woody Lin <wancheng.lin001@gmail.com>
2025-10-30 23:09:08 -0400

    Update StateLifting.jsx

--- react-features/src/components/StateLifting/StateLifting.jsx
+++ react-features/src/components/StateLifting/StateLifting.jsx
@@ -1,10 +1,53 @@
-import React from "react";
+import { useState } from "react";
 import Navigation from "../Navigation";
+
+function InputA({ value, setText }) {
+  return (
+    <div>
+      <h3>InputA</h3>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </div>
+  );
+}
+
+function InputB({ value, setText }) {
+  return (
+    <div>
+      <h3>InputB</h3>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </div>
+  );
+}
+
+function Parent() {
+  const [text, setText] = useState("");
+
+  return (
+    <div>
+      <p>State Lifting</p>
+      <a href="https://github.com/WoodyLinwc/react-features/blob/main/react-features/src/components/StateLifting/StateLifting.jsx">
+        Example Code
+      </a>
+      <InputA value={text} setText={setText} />
+      <InputB value={text} setText={setText} />
+      <p>Shared text: {text}</p>
+    </div>
+  );
+}
 
 const StateLifting = () => {
   return (
     <div>
       <Navigation />
+      <Parent />
     </div>
   );
 };
