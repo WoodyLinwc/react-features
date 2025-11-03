fec50ff0e557f73dc96e6445bc2bb1eeed5e8172
Woody Lin <wancheng.lin001@gmail.com>
2025-11-03 17:25:49 -0500

    Update DataStructure.jsx

--- react-features/src/components/DataStructure/DataStructure.jsx
+++ react-features/src/components/DataStructure/DataStructure.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Navigation from "../Navigation";
+import { NavLink } from "react-router";
 
 const DataStructure = () => {
   return (
     <div>
       <Navigation />
+      <nav style={{ display: "flex", gap: "20px" }}>
+        <NavLink to="/data-structure/array">Array Operation</NavLink>
+        <NavLink to="/data-structure/object">Object Operation</NavLink>
+      </nav>
     </div>
   );
 };
