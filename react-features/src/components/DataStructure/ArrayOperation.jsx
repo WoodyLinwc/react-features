6446edfb8c82219f8b36c5a7fa284262ab695a1b
Woody Lin <wancheng.lin001@gmail.com>
2025-11-03 15:25:25 -0500

    JS

---
+++ react-features/src/components/DataStructure/ArrayOperation.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ArrayOperation = () => {
+  return <div>ArrayOperation</div>;
+};
+
+export default ArrayOperation;
---
+++ react-features/src/components/DataStructure/ObjectOperation.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ObjectMethods = () => {
+  return <div>ObjectMethods</div>;
+};
+
+export default ObjectMethods;
