caee45a60b01784b8d0f56d96a3bb7bbff4d3d24
Woody Lin <wancheng.lin001@gmail.com>
2025-11-04 16:17:51 -0500

    debounce

--- react-features/src/components/DebouncingThrottling/DebouncingThrottling.jsx
+++ react-features/src/components/DebouncingThrottling/DebouncingThrottling.jsx
@@ -1,10 +1,42 @@
 import React from "react";
 import Navigation from "../Navigation";
+// import { debounce, throttle } from "lodash";
+
+// most basic debounce
+function basicDebounce(fn, delay) {
+  let timer;
+
+  // (...args)
+  return function () {
+    clearTimeout(timer);
+    // timer = setTimeout(fn(args), delay);
+    timer = setTimeout(fn, delay);
+  };
+}
+
+function SearchBar() {
+  const handleSeach = basicDebounce(
+    (term) => console.log("Searching for: ", term),
+    2000
+  );
+
+  return <input type="text" onChange={(e) => handleSeach(e.target.value)} />;
+}
 
 const DebouncingThrottling = () => {
   return (
     <div>
       <Navigation />
+      <p>
+        <strong>Debouncing</strong>: delay execution until user stops action
+        (search input)
+      </p>
+      <p>
+        <strong>Throttling</strong>: limit execution rate (scroll, resize
+        events)
+      </p>
+      <p>Prevents excessive function calls, overwhelm APIs, make UI laggy</p>
+      <SearchBar />
     </div>
   );
 };
